e6fc9e2bc73adc3bb5f18808b46bc2131d0f3226
elewis804 <erok84506@gmail.com>
2022-06-03 14:23:01 -0400

    Created runnable finite state machines
    The code for creating and running finite state machines has been created, it just needs to be tested. Also created tests to ensure that the machines can be serialized and deserialized.

--- Easy Machines/src-tauri/src/finite_state_machine.rs
+++ Easy Machines/src-tauri/src/finite_state_machine.rs
@@ -2,9 +2,41 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FiniteStateMachine<'i> {
-    input: &'i str,
-    states: Vec<&'i str>,
-    current_state: &'i str,
-    transitions: Vec<(&'i str, char, &'i str)>,
-    accept_states: Vec<&'i str>
+    pub input: &'i str,
+    pub states: Vec<&'i str>,
+    pub current_state: &'i str,
+    pub transitions: Vec<(&'i str, char, &'i str)>,
+    pub accept_states: Vec<&'i str>
+}
+
+impl<'i> FiniteStateMachine<'i> {
+    pub fn run(&mut self) -> bool {
+        'outer: while self.input.len() > 0 && !self.current_state.eq("NA") {
+            'inner: for t in &self.transitions {
+                if self.current_state.eq(t.0) && t.1.eq(&'\0') {
+                    self.current_state = t.2;
+                }
+                if self.current_state.eq(t.0) && self.input.get(0..1).unwrap().eq(&(t.1).to_string()) {
+                    self.current_state = t.2;
+                    if self.input.len() > 1 {
+                        self.input = self.input.get(1..self.input.len()).unwrap();
+                        continue 'inner;
+                    }
+                    else {
+                        break 'outer;
+                    }
+                }
+            }
+
+            self.current_state = "NA";
+
+        }
+
+        if self.accept_states.contains(&self.current_state) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- Easy Machines/src-tauri/src/main.rs
+++ Easy Machines/src-tauri/src/main.rs
@@ -3,9 +3,44 @@
   windows_subsystem = "windows"
 )]
 
+/* JSON FORMAT
+{
+    "input":"_",
+    "states":
+    [
+      "_",
+      "_",
+      ...
+    ],
+    "current_state":"s0",
+    "transitions":[
+      [
+        "_",
+        "_",
+        "_"
+      ],
+      [
+        "_",
+        "_",
+        "_"
+      ],
+      ...
+    ],
+    "accept_states":
+    [
+      "_",
+      "_",
+      ...
+    ]
+  } */
+
 pub mod finite_state_machine;
 
-use serde::Deserialize;
+use std::ops::Range;
+
+use finite_state_machine::FiniteStateMachine;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 fn main() {
   tauri::Builder::default()
@@ -15,6 +50,58 @@ fn main() {
 }
 
 #[tauri::command]
-fn load_machine(file_name:String) {
-  
+fn load_machine(file_name:String) -> bool{
+  let mut machine: FiniteStateMachine = serde_json::from_str(&file_name).unwrap();
+  machine.run()
+}
+
+#[test]
+fn machines_serialize_correctly() {
+      let mut s = Vec::new();
+      s.push("s0");
+      let mut t = Vec::new();
+      t.push(("s0",'a',"s0"));
+      let mut a = Vec::new();
+      a.push("s0");
+      let machine = FiniteStateMachine {input: "a", states: s, current_state: "s0", transitions: t, accept_states: a };
+      let serialized_machine = serde_json::to_string(&machine).unwrap();
+      println!("{}", serialized_machine);
+}
+
+#[test]
+fn machines_deserialize_correctly() {
+  let serialized_machine = r#"
+  {
+    "input":"a",
+    "states":
+    [
+      "s0"
+    ],
+    "current_state":"s0",
+    "transitions":[
+      [
+        "s0",
+        "a",
+        "s0"
+      ]
+    ],
+    "accept_states":
+    [
+      "s0"
+    ]
+  }"#;
+  let machine: FiniteStateMachine = serde_json::from_str(serialized_machine).unwrap();
+  println!("{}", machine.input);
+  for s in machine.states {
+    println!("{}", s);
+  }
+  println!("{}", machine.current_state);
+  for t in machine.transitions {
+    println!("({}, {}, {})", t.0, t.1, t.2);
+  }
+  for a in machine.accept_states {
+    println!("{}", a);
+  }
+
+  //TODO Write tests for FSM
 }
