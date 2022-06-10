173abd9673005f7ad32881f1b382d02b3b4cc4f9
elewis804 <erok84506@gmail.com>
2022-06-10 09:24:04 -0400

    Update App.tsx

--- Easy Machines/src/App.tsx
+++ Easy Machines/src/App.tsx
@@ -1,8 +1,26 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
+
+  const onButtonClick = () => {
+    console.log("Button clicked.");
+  }
+
   return (
-    <p class="text-4xl text-green-700 text-center py-20">This app is very poggers!</p>
+    <div>
+      <header class="text-4xl text-green-700 text-center py-20">This app is very poggers!</header>
+      <div class="flex items-center grid grid-cols-5 gap-5">
+        <div class="text-center ">1</div>
+        <div class="text-center ">2</div>
+        <div class="text-center ">3</div>
+        <div class="text-center ">4</div>
+        <div class="text-center ">5</div>
+      </div>
+      <div></div>
+      <div>
+        <button onClick={onButtonClick}>Click me</button>
+      </div>
+    </div>
   );
 };
 
